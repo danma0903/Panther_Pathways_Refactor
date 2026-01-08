8bf521b53d8c323591ea86421d6206f978d815bf
Elliot Crowder <elliot.k.crowder@gmail.com>
2026-01-07 22:51:31 -0800

    loading edges from json into memory as a part of the graph

--- graph.js
+++ graph.js
@@ -21,179 +21,181 @@
  */
 
 export class graph {
-  constructor() {
-    this.nodes = [];
-    this.numNodes = 0;
-    this.edges = [];
-  }
+	constructor() {
+		this.nodes = [];
+		this.numNodes = 0;
+		this.edges = [];
+	}
 
-  getNodes() {
-    return this.nodes;
-  }
+	getNodes() {
+		return this.nodes;
+	}
 
-  getNode(nodeName) {
-    for (const curr_node of this.nodes) {
-      if (curr_node.getName() === nodeName) {
-        return curr_node;
-      }
-    }
-    throw new Error("Node not found!");
-  }
+	getNode(nodeName) {
+		for (const curr_node of this.nodes) {
+			if (curr_node.getName() === nodeName) {
+				return curr_node;
+			}
+		}
+		throw new Error("Node not found!");
+	}
 
-  getDistanceBetween(node1Name, node2Name) {
-    const node1 = this.getNode(node1Name);
-    const node2 = this.getNode(node2Name);
-    return node1.getDistanceTo(node2);
-  }
+	getDistanceBetween(node1Name, node2Name) {
+		const node1 = this.getNode(node1Name);
+		const node2 = this.getNode(node2Name);
+		return node1.getDistanceTo(node2);
+	}
 
-  createNode(xCoord, yCoord, nodeName) {
-    const newNode = new graphNode(xCoord, yCoord, nodeName);
-    this.nodes.push(newNode);
-    this.numNodes += 1;
-    //console.log(this.nodes);
-    // Later implement test for duplicates
-  }
+	createNode(xCoord, yCoord, nodeName) {
+		const newNode = new graphNode(xCoord, yCoord, nodeName);
+		this.nodes.push(newNode);
+		this.numNodes += 1;
+		//console.log(this.nodes);
+		// Later implement test for duplicates
+	}
 
-  deleteNode(nodeName) {
-    for (let i = 0; i < this.nodes.length; i++) {
-      if (this.nodes[i].getName() === nodeName) {
-        if (i === this.nodes.length - 1) {
-          this.nodes.pop();
-          this.numNodes -= 1;
-        } else {
-          console.log(this.nodes.length);
-          this.nodes[i] = this.nodes[this.nodes.length - 1];
-          this.nodes.pop();
-          this.numNodes -= 1;
-        }
-      }
-    }
-  }
+	deleteNode(nodeName) {
+		for (let i = 0; i < this.nodes.length; i++) {
+			if (this.nodes[i].getName() === nodeName) {
+				if (i === this.nodes.length - 1) {
+					this.nodes.pop();
+					this.numNodes -= 1;
+				} else {
+					console.log(this.nodes.length);
+					this.nodes[i] = this.nodes[this.nodes.length - 1];
+					this.nodes.pop();
+					this.numNodes -= 1;
+				}
+			}
+		}
+	}
 
-  getNeighborNodes(node) {
-    let neighborNodes = [];
-    for (let i = 0; i < node.edges.length; i++) {
-      if (
-        node.edges[i].node1.getName() === node.getName() ||
-        node.edges[i].node2.getName() === node.getName()
-      ) {
-        neighborNodes.push(node.edges[i].getOppositeNode(node));
-      } else {
-        console.log("no");
-      }
-    }
-    return neighborNodes;
-  }
+	getNeighborNodes(node) {
+		let neighborNodes = [];
+		for (let i = 0; i < node.edges.length; i++) {
+			if (
+				node.edges[i].node1.getName() === node.getName() ||
+				node.edges[i].node2.getName() === node.getName()
+			) {
+				neighborNodes.push(node.edges[i].getOppositeNode(node));
+			} else {
+				console.log("no");
+			}
+		}
+		return neighborNodes;
+	}
 
-  addEdge(weight, node1Name, node2Name) {
-    const node1 = this.getNode(node1Name);
-    const node2 = this.getNode(node2Name);
-    const newEdge = new graphEdge(weight, node1, node2);
-    node1.addEdge(newEdge);
-    node2.addEdge(newEdge);
-    this.edges.push(newEdge);
-  }
-  getEdges() {
-    return this.edges;
-  }
+	addEdge(weight, node1Name, node2Name) {
+		//add duplicate checker?
+		//we should assume that nodes have unique names and that weight is not a key identifier
+		const node1 = this.getNode(node1Name);
+		const node2 = this.getNode(node2Name);
+		const newEdge = new graphEdge(weight, node1, node2);
+		node1.addEdge(newEdge);
+		node2.addEdge(newEdge);
+		this.edges.push(newEdge);
+	}
+	getEdges() {
+		return this.edges;
+	}
 }
 
 class graphNode {
-  constructor(xCoord, yCoord, nodeName) {
-    this.xCoord = xCoord;
-    this.yCoord = yCoord;
-    this.name = nodeName;
-    this.edges = [];
-  }
+	constructor(xCoord, yCoord, nodeName) {
+		this.xCoord = xCoord;
+		this.yCoord = yCoord;
+		this.name = nodeName;
+		this.edges = [];
+	}
 
-  addEdge(edge) {
-    this.edges.push(edge);
-  }
-  getName() {
-    return this.name;
-  }
-  getDistanceTo(node) {
-    //should this function assume that all nodes being requested
-    //to are properly connected?
-    for (const edge of this.edges) {
-      if (
-        edge.getNodes().includes(this.name) &&
-        edge.getNodes().includes(node.getName())
-      ) {
-        return edge.weight;
-      } else {
-        continue;
-      }
-    }
-    return -1;
-  }
+	addEdge(edge) {
+		this.edges.push(edge);
+	}
+	getName() {
+		return this.name;
+	}
+	getDistanceTo(node) {
+		//should this function assume that all nodes being requested
+		//to are properly connected?
+		for (const edge of this.edges) {
+			if (
+				edge.getNodes().includes(this.name) &&
+				edge.getNodes().includes(node.getName())
+			) {
+				return edge.weight;
+			} else {
+				continue;
+			}
+		}
+		return -1;
+	}
 }
 
 class graphEdge {
-  constructor(weight, node1, node2) {
-    this.weight = weight;
-    this.node1 = node1;
-    this.node2 = node2;
-  }
+	constructor(weight, node1, node2) {
+		this.weight = weight;
+		this.node1 = node1;
+		this.node2 = node2;
+	}
 
-  getNodes() {
-    //returns the names of the nodes the edge attaches
-    return [this.node1.getName(), this.node2.getName()];
-  }
-  getOppositeNode(node) {
-    if (
-      node.getName() != this.node1.getName() &&
-      node.getName() != this.node2.getName()
-    ) {
-      throw new Error("Invalid node");
-    }
-    return node.getName() === this.node1.getName() ? this.node2 : this.node1;
-  }
+	getNodes() {
+		//returns the names of the nodes the edge attaches
+		return [this.node1.getName(), this.node2.getName()];
+	}
+	getOppositeNode(node) {
+		if (
+			node.getName() != this.node1.getName() &&
+			node.getName() != this.node2.getName()
+		) {
+			throw new Error("Invalid node");
+		}
+		return node.getName() === this.node1.getName() ? this.node2 : this.node1;
+	}
 }
 
 function Dijkstras(graph, sourceNodeName) {
-  let queue = [];
-  let prev = {};
-  let dists = {};
+	let queue = [];
+	let prev = {};
+	let dists = {};
 
-  for (const node of graph.getNodes()) {
-    dists[node.getName()] = Infinity;
-    prev[node.getName()] = null;
-  }
+	for (const node of graph.getNodes()) {
+		dists[node.getName()] = Infinity;
+		prev[node.getName()] = null;
+	}
 
-  dists[sourceNodeName] = 0;
-  queue.push([0, graph.getNode(sourceNodeName)]);
+	dists[sourceNodeName] = 0;
+	queue.push([0, graph.getNode(sourceNodeName)]);
 
-  //  queue.push([1, "abc"]);
-  // queue.push([5, "blabhba"]);
-  // queue.push([3, "testing333"]);
-  // queue.push([8, "fdsfsdfdd"]);
-  // queue.sort((b, a) => a[0] - b[0]);
-  // queue.pop()
-  //console.log("Sorted queue: ", queue);
+	//  queue.push([1, "abc"]);
+	// queue.push([5, "blabhba"]);
+	// queue.push([3, "testing333"]);
+	// queue.push([8, "fdsfsdfdd"]);
+	// queue.sort((b, a) => a[0] - b[0]);
+	// queue.pop()
+	//console.log("Sorted queue: ", queue);
 
-  while (queue.length > 0) {
-    queue.sort((a, b) => {
-      a[0] - b[0];
-    });
-    const [currentDist, currentNode] = queue.pop();
-    if (currentDist > dists[currentNode.getName()]) {
-      continue;
-    }
-    const currentNodeNeighbors = graph.getNeighborNodes(currentNode);
-    for (neighbor of currentNodeNeighbors) {
-      const weight = graph.getDistanceBetween(
-        currentNode.getName(),
-        neighbor.getName()
-      );
-      const newDist = dists[currentNode.getName()] + weight;
-      if (newDist < dists[neighbor.getName()]) {
-        dists[neighbor.getName()] = newDist;
-        prev[neighbor.getName()] = currentNode.getName();
-        queue.push([newDist, neighbor]);
-      }
-    }
-  }
+	while (queue.length > 0) {
+		queue.sort((a, b) => {
+			a[0] - b[0];
+		});
+		const [currentDist, currentNode] = queue.pop();
+		if (currentDist > dists[currentNode.getName()]) {
+			continue;
+		}
+		const currentNodeNeighbors = graph.getNeighborNodes(currentNode);
+		for (neighbor of currentNodeNeighbors) {
+			const weight = graph.getDistanceBetween(
+				currentNode.getName(),
+				neighbor.getName()
+			);
+			const newDist = dists[currentNode.getName()] + weight;
+			if (newDist < dists[neighbor.getName()]) {
+				dists[neighbor.getName()] = newDist;
+				prev[neighbor.getName()] = currentNode.getName();
+				queue.push([newDist, neighbor]);
+			}
+		}
+	}
 
-  return [dists, prev];
+	return [dists, prev];
 }
--- main.js
+++ main.js
@@ -2,60 +2,72 @@
 
 import { graph } from "./graph.js";
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("Untitled.svg")
-    .then((response) => response.text())
-    .then(async (svgContent) => {
-      console.log("svg content");
-      //   console.log(svgContent);
-      console.log("end content");
-      const parser = new DOMParser();
-      const fileContent = parser.parseFromString(svgContent, "image/svg+xml");
-      await loadNodes(fileContent);
-      const svg = fileContent.querySelector("svg");
-      console.log(svg);
+	fetch("Untitled.svg")
+		.then((response) => response.text())
+		.then(async (svgContent) => {
+			console.log("svg content");
+			//   console.log(svgContent);
+			console.log("end content");
+			const parser = new DOMParser();
+			const fileContent = parser.parseFromString(svgContent, "image/svg+xml");
+			await loadNodes(fileContent);
+			const svg = fileContent.querySelector("svg");
+			console.log(svg);
 
-      //create line group for the line overlay to be displayed
-      const linesGroup = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "g"
-      );
-      linesGroup.id = "lines";
-      //insert the line group at the top of the svg container
-      //we use appendChild because we want the lines to be overlayed above the nodes
-      //which means that they need to be a group placed/ drawn after the node group which is #visible_ellipses
-      svg.appendChild(linesGroup, svg.querySelector("#visible_ellipses"));
-      document.getElementById("svg-container").appendChild(svg);
-    });
+			//create line group for the line overlay to be displayed
+			const linesGroup = document.createElementNS(
+				"http://www.w3.org/2000/svg",
+				"g"
+			);
+			linesGroup.id = "lines";
+			//insert the line group at the top of the svg container
+			//we use appendChild because we want the lines to be overlayed above the nodes
+			//which means that they need to be a group placed/ drawn after the node group which is #visible_ellipses
+			svg.appendChild(linesGroup, svg.querySelector("#visible_ellipses"));
+			document.getElementById("svg-container").appendChild(svg);
+		});
 });
 
+function distance(p1, p2) {
+	const xDifference = p1.xCoord - p2.xCoord;
+	const yDifference = p1.yCoord - p2.yCoord;
+	const sumOfSquares = Math.pow(xDifference, 2) + Math.pow(yDifference, 2);
+	return Math.sqrt(sumOfSquares);
+}
+
 async function loadNodes(fileContent) {
-  let index = 1;
-  let node;
+	let index = 1;
+	let node;
 
-  let mapGraph = new graph();
+	let mapGraph = new graph();
 
-  //while loop iterates through all svg node components and adds the nodes to memory as a part of a graph
-  while ((node = fileContent.getElementById(`node-${index}`))) {
-    mapGraph.createNode(
-      node.getAttribute("cx"),
-      node.getAttribute("cy"),
-      node.id
-    );
-    console.log({
-      id: node.id,
-      cx: node.getAttribute("cx"),
-      cy: node.getAttribute("cy"),
-      r: node.getAttribute("r"),
-    });
-    index++;
-  }
+	//while loop iterates through all svg node components and adds the nodes to memory as a part of a graph
+	while ((node = fileContent.getElementById(`node-${index}`))) {
+		mapGraph.createNode(
+			node.getAttribute("cx"),
+			node.getAttribute("cy"),
+			node.id
+		);
+		console.log({
+			id: node.id,
+			cx: node.getAttribute("cx"),
+			cy: node.getAttribute("cy"),
+			r: node.getAttribute("r"),
+		});
+		index++;
+	}
 
-  //load edge data
-  const edges = await fetch("./edges.json").then((response) => response.json());
-  const TEMPORARY_VALUE = 1;
-  for (let edge of edges.edges) {
-    mapGraph.addEdge(TEMPORARY_VALUE, edge.from, edge.to);
-  }
-  console.log(mapGraph);
-  return graph;
+	//load edge data
+	//DO WE NEED TO ALTER THE CODE to PREVEnt DUPLICATE EDGES FROM BEING CREATED???
+	//EX: It's a problem if we have an edge from a to b and also from b to a because those are the same
+	const edges = await fetch("./edges.json").then((response) => response.json());
+	for (let edge of edges.edges) {
+		const edgeWeight = distance(
+			mapGraph.getNode(edge.from),
+			mapGraph.getNode(edge.to)
+		);
+		mapGraph.addEdge(edgeWeight, edge.from, edge.to);
+	}
+	console.log(mapGraph);
+	return graph;
 }
